2cb8ddce2fbf062307ed26f24aa0a9ac95f9b2e8
dai.father.cat <you@example.com>
2022-12-30 00:57:20 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # construction-mm
+
+GCP mattermost の構築用のスクリプト
+以下のテンプレートを前提とする
+
+[Mattermost Team Edition packaged by Bitnami](https://console.cloud.google.com/marketplace/product/bitnami-launchpad/mattermost)
---
+++ import/link.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# メールアドレス紐付け（末尾のトークンを更新）
+./slack-advanced-exporter --input-archive ./slack_data.zip --output-archive export-with-emails.zip fetch-emails --api-token xoxb-++++++++++
+./slack-advanced-exporter --input-archive export-with-emails.zip --output-archive export-with-emails-and-attachments.zip fetch-attachments
+# mm 用にファイル変換
+./mmetl transform slack --team home-carpentry --file export-with-emails-and-attachments.zip --output slack_export.jsonl
+# 圧縮
+zip -r slack_export.zip data slack_export.jsonl
