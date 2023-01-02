073e56b6f7c8d02b96d8eb3e00093c8c35d57136
dai.father.cat <you@example.com>
2023-01-02 21:21:59 +0900

    refactor: Change comments to English

--- enc/lets-enc.sh
+++ enc/lets-enc.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -ux
 
-# 前提: 以下を事前に実行
+# Install lego in advance. Also, run the following commands
 # sudo /opt/bitnami/letsencrypt/lego --tls --email="sample@sample.co.jp" --domains="two-cats-hc-mm.com" --domains="www.two-cats-hc-mm.com" --path="/opt/bitnami/letsencrypt" run
 
-# 既存の証明書を退避
+# Back up existing certificates
 sudo mv /opt/bitnami/nginx/conf/bitnami/certs/server.crt /opt/bitnami/nginx/conf/bitnami/certs/server.crt.old
 sudo mv /opt/bitnami/nginx/conf/bitnami/certs/server.key /opt/bitnami/nginx/conf/bitnami/certs/server.key.old
 
-# 新しい証明書により上書き
+# Overwrite with a new certificate
 sudo ln -sf /opt/bitnami/letsencrypt/certificates/two-cats-hc-mm.com.key /opt/bitnami/nginx/conf/bitnami/certs/server.key
 sudo ln -sf /opt/bitnami/letsencrypt/certificates/two-cats-hc-mm.com.crt /opt/bitnami/nginx/conf/bitnami/certs/server.crt
 
-# パーミッションの変更
+# Change file permissions
 sudo chown root:root /opt/bitnami/nginx/conf/bitnami/certs/server*
 sudo chmod 600 /opt/bitnami/nginx/conf/bitnami/certs/server*
--- import/link.sh
+++ import/link.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -ux
 
+# Slack API Token
+# ref. <https://katblog.manadream.net/index.php/2022/07/23/mattermost-slack-data-import/#slack_API>
 TOKEN=xoxb-++++++++++
 
-# メールアドレス紐付け（末尾のトークンを更新）
+# Email address linkage
 ./slack-advanced-exporter --input-archive ./slack_data.zip --output-archive export-with-emails.zip fetch-emails --api-token $TOKEN
 ./slack-advanced-exporter --input-archive export-with-emails.zip --output-archive export-with-emails-and-attachments.zip fetch-attachments
-# mm 用にファイル変換
+# Convert data from slack to mattermost
 ./mmetl transform slack --team home-carpentry --file export-with-emails-and-attachments.zip --output slack_export.jsonl
-# 圧縮
+# compression
 zip -r slack_export.zip data slack_export.jsonl
